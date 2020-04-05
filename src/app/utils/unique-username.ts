c56ec9be1acc0d031f57d4e8a95da95dd62e9db8
joseph.gitau <joseph.gitau@canter.fi>
2020-04-05 15:27:12 +0300

    re-configure async validation of username

--- src/app/utils/unique-username.ts
+++ src/app/utils/unique-username.ts
@@ -9,17 +9,35 @@ import { AuthService } from '../auth/auth.service';
 export class UniqueUsername implements AsyncValidator {
   constructor(private authService: AuthService) {}
 
-  validate = (c: AbstractControl) => c.valueChanges.pipe(
-    debounceTime(200),
-    flatMap(value => {
+  validate = (c: AbstractControl) => {
+    const { value } = c;
 
-      return this.authService.isUsernameAvailable(value).pipe(
-        map(() => null), // returns { available: true }
-        catchError(err => {
-          if(err.error.username) return of({noneUniquerUsername: true})
-          return of({noConnection: true})
-        }) // coming from api
-      );
-    })
-  );
+    return this.authService.isUsernameAvailable(value).pipe(
+      map(value => {
+        if (value.available) return null;
+      }),
+      catchError(err => {
+        console.log(err);
+        if (err.error.username) return of({nonUniqueUsername: true});
+        return of({noConnection: true})
+      })
+    );
+  }
+
+  // alternate - cancels multiple requests to api (debouncedTime)
+  // validate = (c: AbstractControl) => c.valueChanges.pipe(
+  //   debounceTime(200),
+
+  //   flatMap(value => {
+  //     return this.authService.isUsernameAvailable(value).pipe(
+  //       map(value => {
+  //         if (value.available) return null;
+  //       }),
+  //       catchError(err => {
+  //         if (err.error.username) return of({nonUniqueUsername: true}); // response variable assigned in api
+  //         return of({noConnection: true}); // handles connection errors
+  //       })
+  //     );
+  //   })
+  // );
 }
