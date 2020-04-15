150e1fe94e6ba392c02cd2f0e3bf2b8e2ff891ac
joseph.gitau <joseph.gitau@canter.fi>
2020-04-15 22:19:38 +0300

    add email-compose modal

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
+import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { EmailCreateComponent } from './email-create/email-create.component';
 import { EmailListComponent } from './email-list/email-list.component';
@@ -12,10 +13,19 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, EmailCreateComponent, EmailListComponent, EmailShowComponent, EmailReplyComponent, EmailPlaceholderComponent, NotFoundComponent],
+  declarations: [
+    HomeComponent,
+    EmailCreateComponent,
+    EmailListComponent,
+    EmailShowComponent,
+    EmailReplyComponent,
+    EmailPlaceholderComponent,
+    NotFoundComponent
+  ],
   imports: [
     CommonModule,
-    InboxRoutingModule
+    InboxRoutingModule,
+    SharedModule
   ]
 })
 export class InboxModule { }
---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ElementRef, OnDestroy, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit, OnDestroy {
+  @Output() dismiss = new EventEmitter();
+
+  constructor(private el: ElementRef) { }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
+  }
+
+  ngOnDestroy() {
+    this.el.nativeElement.remove();
+  }
+
+  onDismiss() {
+    this.dismiss.emit();
+  }
+}
